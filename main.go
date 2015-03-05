21b44a0d6973b7a862afa2c665047149fce10d1b
Felipe Ramos (perenecabuto) <perenecabuto@gmail.com>
2015-03-05 03:39:56 -0300

    Preparing for heroku

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"os"
 	"flag"
 	"log"
 	"net/http"
@@ -16,13 +17,18 @@ func main() {
 		log.Fatal("error starting: ", err)
 	}
 
+	address := config.ServerAddress
+	if len(os.Getenv("PORT")) > 0 {
+        address = "0.0.0.0:" + os.Getenv("PORT")
+	}
+
 	Routes()
 
 	if config.UseTLS {
 		log.Println("Starting Secure WebSocket server at https://" + config.ServerAddress)
 		log.Fatal(http.ListenAndServeTLS(config.ServerAddress, config.CertificateFile, config.KeyFile, nil))
 	} else {
-		log.Println("Starting WebSocket server at http://" + config.ServerAddress)
-		log.Fatal(http.ListenAndServe(config.ServerAddress, nil))
+		log.Println("Starting WebSocket server at http://" + address)
+		log.Fatal(http.ListenAndServe(address, nil))
 	}
 }
